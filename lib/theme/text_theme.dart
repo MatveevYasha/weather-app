992eca0270848ba1b24e6a1895ca60013fbaf462
MatveevYasha <zif666ne@gmail.com>
2023-06-01 10:22:58 +0400

    создал каркас для первого экрана

---
+++ lib/theme/text_theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+TextTheme textTheme = const TextTheme(
+  displayLarge: TextStyle(
+    // fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+    fontSize: 30,
+  ),
+  titleMedium: TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    color: Colors.white,
+  ),
+);
---
+++ lib/ui/weather_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/theme/text_theme.dart';
+import 'package:weather_app/ui/city_search_screen.dart';
+import 'package:weather_app/ui/weather_screen.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Weather App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 0, 0, 0),
+          primary: const Color(0xFF5DB075),
+        ),
+        textTheme: textTheme,
+        useMaterial3: true,
+      ),
+      home: WeatherScreen(),
+    );
+  }
+}
